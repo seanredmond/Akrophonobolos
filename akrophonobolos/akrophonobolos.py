429d521dc9cd34cbc9a0f19904bc1508350c4f9c
Sean Redmond <github-smr@sneakemail.com>
2021-06-15 23:38:36 -0400

    Basic addition and reducing

--- akrophonobolos/akrophonobolos.py
+++ akrophonobolos/akrophonobolos.py
@@ -2,6 +2,37 @@ import re
 
 AMT = re.compile(r"((\d+)T)?((\d+)D)?((\d+(\.\d+)?)O)?")
 
+NUMERALS = {"\U000003a4": (1,   0, 0.0),
+            "\U00010145": (0, 500, 0.0),
+            "\U00000397": (0, 100, 0.0),
+            "\U00000394": (0,  10, 0.0),
+            "\U00010142": (0,   1, 0.0),
+            "\U00000399": (0,   0, 1.0),
+            "\U00010141": (0,   0, 0.5)}
+
+
+def add_amounts(*amts):
+    """ Recursive memberwise addition of tuples. """
+    if len(amts) == 0:
+        return (0, 0, 0)
+
+    return tuple([a + b for a, b in zip(amts[0], add_amounts(*amts[1:]))])
+
+
+def reduce_amount(amt):
+    return _reduce_drachmas((_reduce_obols(amt)))
+
+
+def _reduce_drachmas(amt):
+    return add_amounts((amt[0], 0, amt[2]),
+                       (amt[1] // 6000, amt[1] % 6000, 0))
+
+
+def _reduce_obols(amt):
+    return add_amounts((amt[0], amt[1], 0),
+                       (0, int(amt[2] // 6), amt[2] % 6))
+
+    
 
 def parse_amount(amt):
     """ Parse an Athenian currency string into a tuple. """
@@ -12,3 +43,8 @@ def parse_amount(amt):
     obols = 0 if amt_match[6] is None else float(amt_match[6])
 
     return (talents, drachmas, obols)
+
+
+def parse_greek_amount(amt):
+    """ Parse Unicode Greek acrophonic numeral. """
+    return add_amounts(*[NUMERALS[c] for c in list(amt)])
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -8,3 +8,53 @@ def test_parse_amt():
     assert obol.parse_amount("1T1.5O") == (1, 0, 1.5)
     assert obol.parse_amount("813D1.5O") == (0, 813, 1.5)
     assert obol.parse_amount("1T813D1.5O") == (1, 813, 1.5)
+
+
+def test_parse_greek_amt():
+    assert obol.parse_greek_amount("Τ𐅅ΗΗΗΔ𐅂𐅂𐅂Ι𐅁") == (1, 813, 1.5)
+
+
+def test_add_amounts():
+    assert obol.add_amounts((1, 0, 0), (1, 0, 0)) == (2, 0, 0)
+    assert obol.add_amounts((1, 0, 0), (0, 1, 0)) == (1, 1, 0)
+    assert obol.add_amounts((1, 0, 0), (0, 0, 1)) == (1, 0, 1)
+    assert obol.add_amounts((1, 1, 0), (1, 0, 0)) == (2, 1, 0)
+    assert obol.add_amounts((1, 0, 0), (1, 1, 0)) == (2, 1, 0)
+    assert obol.add_amounts((1, 1, 0), (1, 1, 0)) == (2, 2, 0)
+    assert obol.add_amounts((1, 1, 1), (1, 1, 0)) == (2, 2, 1)
+    assert obol.add_amounts((1, 1, 0), (1, 1, 1)) == (2, 2, 1)
+    assert obol.add_amounts((1, 1, 1), (1, 1, 1)) == (2, 2, 2)
+    
+    assert obol.add_amounts(*(((1, 0, 0),) * 5)) == (5, 0, 0)
+    assert obol.add_amounts(*(((1, 1, 1),) * 5)) == (5, 5, 5)
+
+def test_reduce_amounts():
+    assert obol.reduce_amount((0, 0, 7)) == (0, 1, 1)
+    assert obol.reduce_amount((0, 6001, 0)) == (1, 1, 0)
+    assert obol.reduce_amount((1, 0, 0)) == (1, 0, 0)
+
+    assert obol.reduce_amount((0, 0, 6)) == (0, 1, 0)
+    assert obol.reduce_amount((0, 0, 36000)) == (1, 0, 0)
+
+
+def test_add_with_reducing():
+    a = obol.parse_amount("20T")
+    b = obol.parse_amount("50T")
+    c = obol.parse_amount("28T5610D3.5O")
+    d = obol.parse_amount("44T3000D")
+    e = obol.parse_amount("100T")
+    f = obol.parse_amount("18T3000D")
+
+    assert obol.reduce_amount(
+        obol.add_amounts(a, b, c, d, e, f)) == (261, 5610, 3.5)
+
+
+    u = obol.parse_amount("5696D")
+    v = obol.parse_amount("2T1970D")
+    w = obol.parse_amount("1T1719D2O")
+    x = obol.parse_amount("1T4700D1O")
+    y = obol.parse_amount("3T5940D")
+    z = obol.parse_amount("4173D4O")
+
+    assert obol.reduce_amount(
+        obol.add_amounts(u, v, w, x, y, z)) == (11, 199, 1.0)
